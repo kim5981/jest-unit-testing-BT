c487f067c2b9fe5b7b0f7af542cd4ff6cb2cdd5d
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-25 22:21:58 -0700

    exercise 5

--- index.js
+++ index.js
@@ -84,6 +84,8 @@ class Seasons {
    */
   constructor() {
     // ✨ initialize whatever properties are needed
+    this.seasons = [ "summer", "fall", "winter", "spring"]
+    this.currentSeason = 0
   }
 
   /**
@@ -98,8 +100,15 @@ class Seasons {
    * seasons.next() // returns "spring"
    * seasons.next() // returns "summer"
    */
+  
   next() {
-    // ✨ implement
+  const result = this.seasons[this.currentSeason]
+  if( this.currentSeason === 3 ){
+    this.currentSeason = 0
+  } else {
+    this.currentSeason++
+  }
+  return result
   }
 }
 
